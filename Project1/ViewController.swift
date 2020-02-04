7b4c78e7eb045586ecb77874fa64ab6059123461
Nebi Sarikaya <nebsar@gmail.com>
2020-02-04 01:30:14 -0500

    last commit

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -43,6 +43,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+
         cell.textLabel?.text = "Picture \(indexPath.row + 1) of \(pictures.count)"
         //cell.textLabel?.text = pictures[indexPath.row]
         return cell
